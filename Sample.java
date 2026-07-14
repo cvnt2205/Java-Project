d0a01815b98a10a33cf46d414a8c67fd347945d3
cvnt2205 <153739047+cvnt2205@users.noreply.github.com>
2026-07-14 23:04:46 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,8 +3,7 @@ class Sample
     public static void main (String[] args)
     {
      System.out.println("Welcome to DevOps");
-     System.out.println("Hello from Jenkins");
-      System.out.println("Build and trigger");
+    
     }
   }
 
