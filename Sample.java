a323fbf7da9f8fc0b6fda31f9530619ae39b6b22
cvnt2205 <153739047+cvnt2205@users.noreply.github.com>
2026-07-14 23:14:09 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,8 @@ class Sample
     public static void main (String[] args)
     {
      System.out.println("Welcome to DevOps");
+      System.out.println("Welcome to Jenkins");
+      
     
     }
   }
